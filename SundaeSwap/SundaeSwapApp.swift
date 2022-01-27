b9efd6fe9a4abb76ca1e7328dde789b3864ec477
Raphael Cerqueira <raphaelfc@gmail.com>
2022-01-27 17:50:41 -0300

    Initial Commit

---
+++ SundaeSwap/SundaeSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SundaeSwapApp.swift
+//  SundaeSwap
+//
+//  Created by Raphael Cerqueira on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SundaeSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
